792cd246587ca827e1d624fb90c7bf47d9633072
Freddy Frimpong <fredfrimpong22@gmail.com>
2025-12-23 18:04:30 -0500

    Use PUBLIC_API_URL for Rsbuild

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL =
-  import.meta.env.VITE_API_URL ?? "https://nsaasystems1-app-waofx.ondigitalocean.app/api";
+  process.env.PUBLIC_API_URL || "http://localhost:8080/api";
 
 const api = axios.create({
   baseURL: API_URL,
@@ -9,15 +9,14 @@ const api = axios.create({
 });
 
 api.interceptors.request.use((config) => {
-  if (typeof window !== "undefined") {
-    const role = localStorage.getItem("role");
-    const username = localStorage.getItem("username");
-    const hotelCode = localStorage.getItem("hotelCode");
+  const role = localStorage.getItem("role");
+  const username = localStorage.getItem("username");
+  const hotelCode = localStorage.getItem("hotelCode");
 
-    if (role) config.headers["X-User-Role"] = role;
-    if (username) config.headers["X-Username"] = username;
-    if (hotelCode) config.headers["X-Hotel-Code"] = hotelCode;
-  }
+  if (role) config.headers["X-User-Role"] = role;
+  if (username) config.headers["X-Username"] = username;
+  if (hotelCode) config.headers["X-Hotel-Code"] = hotelCode;
+
   return config;
 });
 
